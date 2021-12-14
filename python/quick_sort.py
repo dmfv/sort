17e25701307b5caf2fb3c99396396ed45c703d9e
dmfv <56220925+dmfv@users.noreply.github.com>
2021-12-14 16:35:03 +0300

    add quick sorting
    first implementation of quick sorting

---
+++ python/quick_sort.py
@@ -0,0 +1,26 @@
+import random
+
+import random
+
+def qSort (unsortedList):
+    if len(unsortedList) < 2:
+        return unsortedList
+    referenceElem = unsortedList.pop(int(len(unsortedList)/2))
+    lessThanRef = []
+    moreThanRef = []
+    # print(referenceElem)
+    for i in unsortedList:
+        if i < referenceElem:
+            lessThanRef.append(i)
+        else:
+            moreThanRef.append(i)   
+    return (qSort(lessThanRef) + [referenceElem] + qSort(moreThanRef))
+
+if __name__ == "__main__":
+    listLen = 100
+    dummyList = [random.randint(1, 100) for i in range(listLen)]
+    print(dummyList)
+    print()
+    dummyList = qSort(dummyList)
+    print()
+    print(dummyList)
